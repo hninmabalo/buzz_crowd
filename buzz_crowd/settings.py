33cdda2a1ebf9e9c0b8986bc625bf46488fa38f8
hninmabalo <hninmabalo@gmail.com>
2022-12-13 22:56:01 -0800

    feat: create Post model and migrate

--- buzz_crowd/settings.py
+++ buzz_crowd/settings.py
@@ -35,7 +35,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'main_app.apps.MainAppConfig',
+    'main_app',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -80,8 +80,8 @@ WSGI_APPLICATION = 'buzz_crowd.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'buzz_crowd',
     }
 }
 
--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,14 @@
+import datetime
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Post(models.Model):
+    user = models.CharField(max_length=100)
+    image = models.FileField(upload_to='images/', blank=True, null=True)
+    content = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField('date created')
+    likes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.user
