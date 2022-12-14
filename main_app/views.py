ce9d54d8b674973a6dcbba6baa28bbea923c7f81
hninmabalo <hninmabalo@gmail.com>
2022-12-13 20:23:19 -0800

    feat: add index, about, base html files templates

--- main_app/views.py
+++ main_app/views.py
@@ -4,4 +4,9 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Buzz_Crowd')
+    return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+
