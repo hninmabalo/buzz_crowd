2e6084765853974b660422b7363bb4c7a28f415f
hninmabalo <hninmabalo@gmail.com>
2022-12-14 21:56:08 -0800

    feat: add profile template

--- main_app/views.py
+++ main_app/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+
 from django.http import HttpResponse
-from .models import Post
+from .models import Post, Profile
 
 # Create your views here.
 def index(request):
-    posts = list(Post.objects.all())
-    return render(request, 'index.html', { 'posts': posts })
+  posts = list(Post.objects.all())
+  return render(request, 'index.html', { 'posts': posts })
 
 def about(request):
-    return render(request, 'about.html')
+  return render(request, 'about.html')
 
-
+def profile(request, pk):
+  profiles = list(Profile.objects.all())
+  return render(request, 'profile.html', { 'profiles': profiles})
