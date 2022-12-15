80d5137329131c3bc3f39febc6bbf441ef56a142
hninmabalo <hninmabalo@gmail.com>
2022-12-14 18:46:40 -0800

    feat: add index template

--- buzz_crowd/settings.py
+++ buzz_crowd/settings.py
@@ -35,7 +35,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'main_app',
+    'main_app.apps.MainAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- main_app/views.py
+++ main_app/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Post
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    posts = list(Post.objects.all())
+    return render(request, 'index.html', { 'posts': posts})
 
 def about(request):
     return render(request, 'about.html')
