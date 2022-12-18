c40ff2e6b7fbb9e972b2044525b5b415c240ab98
hninmabalo <hninmabalo@gmail.com>
2022-12-17 23:01:33 -0800

    chore: add signup feature

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
 from django.http import HttpResponseRedirect
 from django.http import HttpResponse
 from .models import Post, Profile
@@ -8,41 +9,73 @@ from .models import Post, Profile
 # Create your views here.
 def index(request):
 
-  # user_object = User.objects.get(username=request.user.username)
-  # user_profile = Profile.objects.get(user=user_object)
+    # user_object = User.objects.get(username=request.user.username)
+    # user_profile = Profile.objects.get(user=user_object)
 
-  posts = list(Post.objects.all())
+    posts = list(Post.objects.all())
 
-  return render(request, 'index.html', { 'posts': posts })
+    return render(request, 'index.html', {'posts': posts})
+
 
 def about(request):
-  return render(request, 'about.html')
+    return render(request, 'about.html')
+
 
 def create_post(request):
-  
-  if request.method == 'POST':
-    user = request.user.username
-    image = request.FILES.get('image_upload')
-    content = request.POST['content']
 
-    new_post = Post.objects.create(user=user, image=image, content=content)
-    new_post.save()
+    if request.method == 'POST':
+        user = request.user.username
+        image = request.FILES.get('image_upload')
+        content = request.POST['content']
 
-    return  HttpResponseRedirect('/')
-  else:
-    return  HttpResponseRedirect('/')
+        new_post = Post.objects.create(user=user, image=image, content=content)
+        new_post.save()
+
+        return HttpResponseRedirect('/')
+    else:
+        return HttpResponseRedirect('/')
+
 
 def profile(request, pk):
-  profiles = list(Profile.objects.all())
-  return render(request, 'profile.html', { 'profiles': profiles })
+    profiles = list(Profile.objects.all())
+    return render(request, 'profile.html', {'profiles': profiles})
+
 
 def signup(request):
-  return render(request, 'signup.html')
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        if password == password2:
+            if User.objects.filter(email=email).exists():
+                messages.info(request, 'Email Exists')
+                return HttpResponseRedirect('/signup/')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, 'Username Exists')
+                return HttpResponseRedirect('/signup/')
+            else:
+                user = User.objects.create_user(
+                    username=username, email=email, password=password)
+                user.save()
+
+                user_model = User.objects.get(username=username)
+                new_profile = Profile.objects.create(user=user_model, id_user=user_model.id)
+                new_profile.save()
+                return HttpResponseRedirect('/login/')
+        else:
+            messages.info(request, 'Password Does Not Match')
+            return HttpResponseRedirect('/signup/')
+    else:
+        return render(request, 'signup.html')
+
 
 def login(request):
-  return render(request, 'login.html')
+    return render(request, 'login.html')
+
 
 def logout(request):
-  logout(request)
-  return HttpResponseRedirect('/')
-
+    logout(request)
+    return HttpResponseRedirect('/')
